66ba1004a9c1afd1aa942fc4cc0ac0abd9ea45dc
Augusto Gutierrez <augustogu.contact@gmail.com>
2025-04-05 00:19:40 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
   title = 'clase-3-angular-2023';
+  unSaludo: string = 'Este es un saludo desde Typescript';
+  numero = 0;
+  hayError = true;
+  h4 ={
+    fontSize: '2rem',
+    backgroundColor: '#fff',
+    color:' #1c1c1c'
+  }
+
+  constructor(){
+    setInterval(() =>{
+      this.numero++;
+    }, 1000);
+  }
+
 }
